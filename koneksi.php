66e9797889cb536c614e087100c4af02a063bb28
root <root@uas-24010314140074.sub06261511340.uas240103141400.oraclevcn.com>
2020-06-27 08:45:34 +0000

    AROBW

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = "";
+$db_pass = "palembang27";
 $db_name = "raport";
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
